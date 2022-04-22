92acd18298cf2d4d80d73efc85f5fdbfe80e81ee
wangqingbin001 <wangqingbin001@ke.com>
2022-04-22 23:27:17 +0800

    cv2

---
+++ core/exec.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File   : exec.py
+@Time   : 2022/04/22 01:54:25
+@Author : Frieza 
+@Desc   : 
+'''
+
+
+class A:
+    pass
+
+
+if __name__ == '__main__':
+    print()
+    print('sdfsdfsfsf')
+
